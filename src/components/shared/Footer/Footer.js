e352180f11b9bd9c70e710d5d6d63f074cf63e2e
Justin Clarke <justinclarke241@gmail.com>
2021-08-09 13:58:02 +0530

    Revert "🚀"

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -1,70 +1,7 @@
 import React from "react";
-import { BsChevronRight, BsList } from "react-icons/bs";
 
 const Footer = () => {
-  return(
-    <footer id="footer">
-      <div className="footer-top">
-        <div className="container">
-          <div className="flex h-screen">
-            <div className="flex-1 m-auto footer-contact">
-              <p>
-                <span style={{fontSize: '30px'}}>LiteStore</span>
-                <br />#58, Domlur Layout,
-                <br />Bangalore-71<br />
-                <strong>Phone: <br /> </strong> +91 9483465923<br />
-                <span>+91 6363294498</span> <br />
-                <strong>Email:</strong> support@litestore.in<br />
-              </p>
-            </div>
-            <div className="flex-1 m-auto footer-links">
-              <h4>How it works</h4>
-              <ul>                            
-                <li> <a href="list-your-space.html">List a Space</a></li>
-                <li> <a href="find-a-space.html">Find a Space</a></li>
-                <li> <a href="services.html">LiteStore Services</a></li>
-              </ul>
-            </div>
-            <div className="flex-1 m-auto footer-links">
-              <h4>About</h4>
-              <ul>
-                <li> <a href="careers.html">Careers</a></li>
-                <li> <a href="company.html">Company</a></li>
-                <li> <a href="faqs/faq-section.html">FAQs</a></li>
-              </ul>
-            </div>
-            <div className="flex-1 m-auto footer-links">
-              <h4>Discover</h4>
-              <ul>
-                <li> <a href="blogs/list.html">Blog</a></li>
-                <li> <a href="community.html">Community</a></li>
-                <li> <a href="design.html">DesignHub</a></li>
-              </ul>
-            </div>
-            <div className="flex-1 m-auto footer-links">
-              <h4>Legal</h4>
-              <ul>
-                <li> <a href>Privacy Policy</a></li>
-                <li> <a href>Terms of use</a></li>
-                <li> <a href="dashboard/html/index.html">Dashboard</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container d-md-flex py-4" style={{backgroundColor: '#fff'}}>
-        <div className="">
-          <div className="copyright">
-            © Copyright 2021 <strong><span>LiteStore</span></strong>. All Rights Reserved
-          </div>
-        </div>
-        <div className="social-links text-center text-md-right pt-3 pt-md-0" style={{textAlign: "end"}}>
-          <a href="blank.html" className="instagram"><i className="bx bxl-instagram" /></a>
-          <a href="https://www.linkedin.com/company/litestore/" className="linkedin"><i className="bx bxl-linkedin" /></a>
-        </div>
-      </div>
-    </footer>
-  );
-}
+  return <div>fdg</div>;
+};
 
 export default Footer;
