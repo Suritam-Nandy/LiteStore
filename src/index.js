8fdcd64d1cd05da4a5c32499a5c6779f0c126156
Justin Clarke <justinclarke241@gmail.com>
2021-08-24 17:01:18 +0530

    🚀

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import Navbar from "./components/shared/Navbar/Navbar";
 import App from "./App";
-// import Footer from "./components/shared/Footer/Footer";
 
 import { BrowserRouter } from "react-router-dom";
 
