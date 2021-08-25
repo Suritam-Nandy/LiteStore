c79a7ac757269dfdc5cf535e028e59824b449953
Suritam-Nandy <13suritsuritam@gmail.com>
2021-08-25 13:51:01 +0530

    ✨

--- src/components/shared/static pages/ListASpace.js
+++ src/components/shared/static pages/ListASpace.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 
 const ListASpace = () => {
   return (
     <div>
-      <Helmet>
+      {/* <Helmet>
         <title>List A Space | LiteStore</title>
-      </Helmet>
+      </Helmet> */}
       {/*  Heading  */}
       <section
         className="head-section"
