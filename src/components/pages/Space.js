8a62c9603c5aadd62b72fa1fd4062721f43b77f2
Suritam-Nandy <13suritsuritam@gmail.com>
2021-09-02 21:37:55 +0530

    ✨

--- src/components/pages/Space.js
+++ src/components/pages/Space.js
@@ -27,41 +27,8 @@ const Space = () => {
   if (space == null) {
     return <Loading />;
   }
-  return (
-    <div className="container">
-      {/* <div className="py-4">
-        <div className="row">
-          <div className="col-md-10 mx-auto">
-            <div className="card card-body shadow">
-              <div className="row">
-                <div className="col-md-4">
-                  <Avatar url={`https://i.pravatar.cc/150?img=${id}`} />
-                </div>
-                <div className="col-md-8">
-                  <ul className="list-group">
-                    <li className="d-flex justify-content-between align-items-center list-group-item list-group-item-action">
-                      <h3 className="m-0">{space.name}</h3>
-                      <Link className="btn btn-primary" to={`/spaceForm/${id}`}>
-                        edit profile
-                      </Link>
-                    </li>
-                    <li className="list-group-item">
-                      <p>email: {space.email}</p>
-                      <p>phone: {space.phone}</p>
-                      <p>class: {space.standard}</p>
-                      <p>address 1: {space.address1}</p>
-                      <p>address 2: {space.address2}</p>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
-      {space.area}
-    </div>
-  );
+  console.log(space.area);
+  return <div className="mt-20">{space.area}</div>;
 };
 
 export default Space;
