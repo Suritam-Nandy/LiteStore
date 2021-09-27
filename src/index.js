e35d6e68f58694c125d7fc99fedcd0a673746e5c
Suritam-Nandy <13suritsuritam@gmail.com>
2021-09-27 11:29:01 +0530

    💄

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
-import "./index.css";
-// import Navbar from "./components/shared/Navbar/Navbar";
-=======
->>>>>>> 8fdcd64d1cd05da4a5c32499a5c6779f0c126156
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
