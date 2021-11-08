e08f710073625aff3b154da8b273512414004500
Suritam-Nandy <13suritsuritam@gmail.com>
2021-11-08 22:48:15 +0530

    ⚡️

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -6,7 +6,7 @@ const Dashboard = () => {
     <div className="">
       <div className="flex flex-wrap overflow-hidden max-h-full ">
         <Sidebar />
-        <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6  mt-16">
+        <div className="container mx-auto py-10 h-64 md:w-4/5 xl:w-10/12 px-6  mt-16">
           <div className="my-2">
             <h1 className="text-2xl">Home</h1>
           </div>
